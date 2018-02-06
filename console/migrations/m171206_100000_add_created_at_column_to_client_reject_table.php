95fa5421508edf285d9f31cb810a58c8422e89dc
dvs <valerok365@mail.ru>
2018-02-06 17:09:46 +0400

    #bag fix in  migrate

--- console/migrations/m171206_100000_add_created_at_column_to_client_reject_table.php
+++ console/migrations/m171206_100000_add_created_at_column_to_client_reject_table.php
@@ -9,7 +9,7 @@ class m171206_100000_add_created_at_column_to_client_reject_table extends Migrat
      */
     public function up()
     {
-        $this->addColumn('{{%client_reject}}', 'created_at', $this->timestamp());
+        $this->addColumn('{{%client_reject}}', 'created_at', $this->timestamp()->notNull()->defaultExpression('CURRENT_TIMESTAMP'));
     }
 
     /**
